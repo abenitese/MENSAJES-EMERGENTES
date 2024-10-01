489db6865960dde15c048a614d55f3b62c1a42f4
Alex Benites E. <66579129+abenitese@users.noreply.github.com>
2024-10-01 00:14:04 -0500

    Add files via upload

---
+++ js/mensajes.js
@@ -0,0 +1,11 @@
+//var respuesta = window.prompt("Tienes Cuenta?");
+
+//alert("su respuesta es: " + respuesta);
+
+var confirmacion = window.confirm("Confirmar Registro?")
+
+if (confirmacion == true) {
+    alert("REGISTRO COMPLETO");
+} else {
+    alert("SE CANCELO EL REGISTRO");
+}
